09689968588c5d5501339fa1aad6fc804c92f4e2
moovlaze <max.moovlaze@gmail.com>
2025-01-02 17:38:27 +0300

    changes

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from products.models import ProductCategory, Product
+
+
+def index(request):
+    context = {"title": "test title"}
+    return render(request, "products/index.html", context=context)
+
+
+def products(request):
+    context = {
+        "products": Product.objects.all(),
+        "categories": ProductCategory.objects.all()
+    }
+
+    return render(request, "products/products.html", context=context)
